97254283ac56337bbc6cf0bd7cf2d60b5dad6d8c
ALINA <130515756+AfAlina190@users.noreply.github.com>
2024-02-25 14:52:30 +0100

    11

--- js/task-1.js
+++ js/task-1.js
@@ -1,27 +1,3 @@
-// Отримуємо доступ до елементу ul#categories
-const categoriesList = document.getElementById('categories');
-
-// Отримуємо всі елементи li.item в межах ul#categories
-const categoriesItems = categoriesList.querySelectorAll('li.item');
-
-// Виводимо кількість категорій
-console.log('Number of categories:', categoriesItems.length);
-
-// Проходимося по кожному елементу li.item
-categoriesItems.forEach((categoryItem) => {
-  // Отримуємо заголовок категорії
-  const categoryName = categoryItem.querySelector('h2').textContent;
-
-  // Отримуємо всі елементи li в межах поточної категорії
-  const categoryElements = categoryItem.querySelectorAll('li');
-
-  // Виводимо інформацію про категорію
-  console.log(`Category: ${categoryName}\nElements: ${categoryElements.length}\n`);
-});
-
-
-
-
 document.addEventListener("DOMContentLoaded", function () {
   // Знаходимо елемент ul#categories
   const categoriesList = document.getElementById("categories");
